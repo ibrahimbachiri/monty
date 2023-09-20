a42c1b4e354ed57d5cf82520c4551e55ecd5e15c
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-20 01:03:53 -0700

    m

--- pop.c
+++ pop.c
@@ -11,18 +11,18 @@
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp;
+	stack_t *temp;
 
-    if (!stack || !*stack)
-    {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (!stack || !*stack)
+	{
+	fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+	exit(EXIT_FAILURE);
+	}
 
-    temp = *stack;
-    *stack = (*stack)->next;
-    if (*stack)
-        (*stack)->prev = NULL;
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack)
+	(*stack)->prev = NULL;
 
-    free(temp);
+	free(temp);
 }
