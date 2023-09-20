dc095ba815ae2cac94c3ae2a446b00f089e737b2
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-20 00:57:32 -0700

    m

--- monty.h
+++ monty.h
@@ -39,6 +39,7 @@ typedef struct instruction_s
 void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, int line_number, int value);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 
 /* Add more function prototypes for other opcodes as needed */
 
