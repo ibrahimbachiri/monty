723aa4f05edbc0aa0b99ed6e5288623e3007e526
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-20 00:07:41 -0700

    monty

---
+++ opcode.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/* Function to push an element onto the stack */
+void push(stack_t **stack, int line_number, int value)
+{
+    stack_t *new_node;
+(void)line_number;
+    new_node = malloc(sizeof(stack_t));
+    if (!new_node)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+
+    if (*stack)
+        (*stack)->prev = new_node;
+
+    *stack = new_node;
+}
