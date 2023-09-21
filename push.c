a20316d08cc53cc5a4a764a79e852f2b607c1ae0
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-21 05:52:55 -0700

    m

--- push.c
+++ push.c
@@ -1,37 +1,98 @@
 #include "monty.h"
-/**
- * f_push - add node to the stack
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_push(stack_t **head, unsigned int counter)
-{
-	int n, j = 0, flag = 0;
 
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			j++;
-		for (; bus.arg[j] != '\0'; j++)
-		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
+/**
+ * push - Pushes an element onto the stack
+ * @stack: Pointer to the pointer to the stack (doubly linked list)
+ * @line_number: Line number in the Monty file
+ *
+ * Description: Pushes an integer onto the stack. If not an integer or no
+ * argument given, prints an error and exits with EXIT_FAILURE.
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+    char *arg;
+    int value;
+
+    arg = strtok(NULL, " \t\n");
+
+    if (!arg || !is_number(arg))
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    value = atoi(arg);
+
+    /* Create a new node and push it onto the stack */
+    push_node(stack, value);
+}
+
+/**
+ * is_number - Checks if a string is a valid integer
+ * @str: The string to check
+ *
+ * Return: 1 if it's a number, 0 otherwise
+ */
+int is_number(char *str)
+{
+    int i = 0;
+
+    if (str[0] == '-')
+        i = 1;
+
+    for (; str[i] != '\0'; i++)
+    {
+        if (!isdigit(str[i]))
+            return (0);
+    }
+    return (1);
+}
+
+/**
+ * pall - Prints all the values on the stack
+ * @stack: Pointer to the pointer to the stack (doubly linked list)
+ * @line_number: Line number in the Monty file
+ *
+ * Description: Prints all the integer values on the stack from top to bottom.
+ * If the stack is empty, don't print anything.
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *current = *stack;
+
+    (void)line_number;
+
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
+
+
+/**
+ * push_node - Adds a new node to the stack
+ * @stack: Pointer to the pointer to the stack (doubly linked list)
+ * @value: Value to be added to the new node
+ *
+ * Description: Creates a new node, assigns the value, and adds it to the top
+ * of the stack.
+ */
+void push_node(stack_t **stack, int value)
+{
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (!new_node)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+
+    if (*stack)
+        (*stack)->prev = new_node;
+
+    *stack = new_node;
 }
