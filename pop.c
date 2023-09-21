9b203f19d4d5fd42a261d622cc067f9c45dd6fc1
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-21 09:00:17 -0700

    m

--- pop.c
+++ pop.c
@@ -6,14 +6,16 @@
  * pop - Removes the top element of the stack.
  * @stack: A pointer to the stack.
  *
- * This function removes the top element of the stack. If the stack is empty,
+ * This function removes the top element of the stack.
  * it prints an error message and exits with EXIT_FAILURE.
  */
 
-void pop(Stack *stack) {
-    if (stack->top == -1) {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    stack->top--;
+void pop(Stack *stack)
+{
+	if (stack->top == -1)
+	{
+	fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+	exit(EXIT_FAILURE);
+	}
+	stack->top--;
 }
