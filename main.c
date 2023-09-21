6f0f582cec69f118bb270348594d0edc7980ec0c
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-21 08:04:20 -0700

    m

--- main.c
+++ main.c
@@ -3,40 +3,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    FILE *file;
-    Stack stack;
-    char opcode[100];
-    int value;
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	Stack stack;
+	char opcode[100];
+	int value;
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+	if (argc != 2) {
+	fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+	return EXIT_FAILURE;
+	}
 
-    file = fopen(argv[1], "r");
-    if (file == NULL) {
-        perror("Unable to open file");
-        return EXIT_FAILURE;
-    }
+	file = fopen(argv[1], "r");
+	if (file == NULL) {
+	perror("Unable to open file");
+	return EXIT_FAILURE;
+	}
 
-    stack.top = -1;
+	stack.top = -1;
 
-    while (fscanf(file, "%s", opcode) != EOF) {
-        if (strcmp(opcode, "push") == 0) {
-            if (fscanf(file, "%d", &value) != 1) {
-                fprintf(stderr, "Error: push requires an integer argument\n");
-                return EXIT_FAILURE;
-            }
-            push(&stack, value);
-        } else if (strcmp(opcode, "pall") == 0) {
-            pall(&stack);
-        } else {
-            fprintf(stderr, "Unknown opcode: %s\n", opcode);
-            return EXIT_FAILURE;
-        }
-    }
+	while (fscanf(file, "%s", opcode) != EOF) {
+	if (strcmp(opcode, "push") == 0) {
+	if (fscanf(file, "%d", &value) != 1) {
+	fprintf(stderr, "Error: push requires an integer argument\n");
+	return EXIT_FAILURE;
+	}
+	push(&stack, value);
+	}
+	else if (strcmp(opcode, "pall") == 0) {
+	pall(&stack);
+	}
+	else
+	{
+	fprintf(stderr, "Unknown opcode: %s\n", opcode);
+	return EXIT_FAILURE;
+	}
+	}
 
-    fclose(file);
-    return EXIT_SUCCESS;
+	fclose(file);
+	return EXIT_SUCCESS;
 }
