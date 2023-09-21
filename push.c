77159e438e1a367904e572cc9e0dab4516ec7080
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-21 06:53:10 -0700

    m

--- push.c
+++ push.c
@@ -1,9 +1,21 @@
 #include "monty.h"
-void push(Stack* stack, int value) {
-    if (stack->top < STACK_SIZE - 1) {
-        stack->data[++(stack->top)] = value;
-    } else {
-        fprintf(stderr, "Stack overflow\n");
-        exit(EXIT_FAILURE);
-    }
+/**
+ * push - Pushes an integer onto the stack.
+ * @stack: Pointer to the stack data structure.
+ * @value: The integer value to be pushed.
+ *
+ * This function pushes the specified integer onto the stack
+ * if there is space available. If the stack is full, it
+ * prints an error message and exits with EXIT_FAILURE.
+ */
+void push(Stack* stack, int value)
+{
+	if (stack->top < STACK_SIZE - 1) {
+	stack->data[++(stack->top)] = value;
+	}
+	else
+	{
+	fprintf(stderr, "Stack overflow\n");
+	exit(EXIT_FAILURE);
+	}
 }
