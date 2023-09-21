e0f380f020778b524223b3a0a739c01d0df5a523
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-09-21 06:10:13 -0700

    m

--- pint.c
+++ pint.c
@@ -11,11 +11,11 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-    if (!stack || !*stack)
-    {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (!stack || !*stack)
+	{
+	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+	exit(EXIT_FAILURE);
+	}
 
-    printf("%d\n", (*stack)->n);
+	printf("%d\n", (*stack)->n);
 }
